0333cfa92163268cabfe4a7f2cafc593eed556aa
preethi-B92 <112534092@kanchiuniv.ac.in>
2026-02-18 15:37:16 +0530

    Add files via upload

---
+++ insertionsort.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int i,n,key,j;
+	printf("Enter number of elements:");
+	scanf("%d",&n);
+	int arr[n];
+	printf("Enter %d element:\n",n);
+	 for(i=0;i<n;i++)
+	scanf("%d",&arr[i]);
+	//insertion sort logic
+	for(i=1;i<n;i++)
+	{
+		key =arr[i];
+		j=i-1;
+		while(j>=0&& arr[j]>key)
+		{
+			arr[j+1]=arr[j];
+			j--;
+		}
+		printf("sorted array:\n");
+		for(i=0;i<n;i++)
+		printf("%d",arr[i]);
+		return 0;
+		}
+		
+	}
+
